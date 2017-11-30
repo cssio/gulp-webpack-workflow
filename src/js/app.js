9b469c3f80b62cd82e978e2b94de8e8cfe08e036
cssio <vlad.muraviov@gmail.com>
2017-11-30 19:13:50 +0200

    v.0.0.1

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+// import {$, jQuery} from 'jquery';
+
+
+// import owlCarousel from './jquery.owl.carousel.js';
+// import owlCarousel from 'owl.carousel'
+import scripts from './scripts';
+
+
+// require('./scripts.js')
+
---
+++ src/js/scripts.js
@@ -0,0 +1,113 @@
+// export default function scripts() {
+
+	$(document).click(function() {
+
+		let hello = 'hello webpack'
+	  	alert(hello)
+	});
+
+
+	// 	$('.owl-carousel').owlCarousel({
+	//     loop: true,
+	//     margin: 10,
+	//     nav: false,
+	//     dotsEach: true,
+	//     responsive:{
+	//         0:{
+	//             items:1
+	//         },
+	//         768:{
+	//             items:3
+	//         }
+	//     }
+	// });
+
+// }
+
+
+
+// require('modules');
+// requare('jquery-3.2.1.min.js');
+
+// $(function() {
+
+	// alert('hello')
+
+// });
+// 	// --------------------------------------------------------------------------
+// 	// SVG
+// 	// --------------------------------------------------------------------------
+
+// 	svg4everybody();
+
+// 	// --------------------------------------------------------------------------
+// 	// Formstyler
+// 	// --------------------------------------------------------------------------
+
+// 	$('.app-select').styler({
+// 		selectSmartPositioning: false,
+// 		selectSearch: false,
+// 		selectVisibleOptions: 0,
+// 		selectSearchLimit: 6
+// 	});
+
+// 	// --------------------------------------------------------------------------
+// 	// Owl Carousel
+// 	// --------------------------------------------------------------------------
+
+
+// 	$('.owl-carousel').owlCarousel({
+// 	    loop: true,
+// 	    margin: 10,
+// 	    nav: false,
+// 	    dotsEach: true,
+// 	    responsive:{
+// 	        0:{
+// 	            items:1
+// 	        },
+// 	        768:{
+// 	            items:3
+// 	        }
+// 	    }
+// 	});
+
+
+// 	// --------------------------------------------------------------------------
+// 	// Mask Phone
+// 	// --------------------------------------------------------------------------
+
+// 	$(".is-mask-phone").mask("+7 (999) 999-9999");
+
+
+// 	// --------------------------------------------------------------------------
+// 	// Range
+// 	// --------------------------------------------------------------------------
+
+// 	$('.app-range-input').ionRangeSlider({
+// 		type: "double",
+// 		from: 0,
+// 		min: 0,
+// 		max: 1000,
+// 		step: 5,
+// 		hide_min_max: true,
+// 		hide_from_to: true,
+// 		force_edges: true,
+// 		grid: false
+// 	});
+
+
+// 	$('.app-range-input').on('change', function(event) {
+// 		event.preventDefault();
+
+// 		var range = $(this),
+//         	rangeData = range.data("ionRangeSlider"),
+//         	rangeDataFrom = range.data("from"),
+//         	rangeDataTo = range.data("to");
+
+//         range.closest('.app-range').find('.app-range-data').text(rangeDataFrom + ' - ' + rangeDataTo)
+
+// 	});
+
+
+
+// });
